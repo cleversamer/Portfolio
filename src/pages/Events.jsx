fd817aae08df06265db17ad8473ce7f0d709adfd
Samer A <thedev.samer@gmail.com>
2022-06-10 22:57:36 +0300

    finished building the evets page

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -3,17 +3,28 @@ import Navbar from "../components/Navbar";
 import Content from "../components/Content";
 
 const Events = () => {
+  const events = [];
+
   return (
     <Container>
       <Navbar path="/events" />
 
       <Content>
-        <h1>Hello World!</h1>
+        {events.length ? (
+          <Heading>{events.length} upcoming events!</Heading>
+        ) : (
+          <Heading>No upcoming events yet...</Heading>
+        )}
       </Content>
     </Container>
   );
 };
 
 const Container = styled.div``;
+
+const Heading = styled.h3`
+  text-align: center;
+  color: #333;
+`;
 
 export default Events;
